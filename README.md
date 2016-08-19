8c731864eb108183d8cf0aa7c4b6d42dfb8c41e0
Alex <ac@barcodepublicity.com>
2016-08-18 23:41:24 -0400

    Update build

---
+++ README.md
@@ -0,0 +1,5 @@
+# Docker Cloud + RDS Implementation for site creation
+
+This code builds the docker installation for a basic web app.
+
+
--- up.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-FOR /f "tokens=*" %%i IN ('docker ps -a -q') DO docker rm -f %%i
-docker build -t tehraven/dark .
-docker run -p 80:80 tehraven/dark
