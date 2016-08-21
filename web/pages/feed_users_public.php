b12ce8fa90ebfa150554a05d2318272ece9bdc1c
Alex <ac@barcodepublicity.com>
2016-08-21 16:56:43 -0400

    Testing Stripe

--- web/pages/feed_users_public.php
+++ web/pages/feed_users_public.php
@@ -5,6 +5,19 @@
         <span class="dark-right dark-opacity">16 min</span>
         <h4>Jane Doe</h4><br>
         <hr class="dark-clear">
+        <p>
+        <form action="/create_subscription.php" method="POST">
+  <script
+    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+    data-key="pk_test_kkoq32TRFRxxf5IrQgTZgKhE"
+    data-image="images/marketplace.png"
+    data-name="Emma's Farm CSA"
+    data-description="Subscription for 1 weekly box"
+    data-amount="2000"
+    data-label="Sign Me Up!">
+  </script>
+</form>
+        </p>
         <p><?php print_r($dark['profile']); ?></p>     <button type="button" class="dark-btn dark-theme-d1 dark-margin-bottom"><i class="fa fa-thumbs-up"></i>  Like</button>
         <button type="button" class="dark-btn dark-theme-d2 dark-margin-bottom"><i class="fa fa-comment"></i>  Comment</button>
       </div>
